a13b8857d045f0cf3ac0cee8aa1707fe1c1a247c
Pravin671231 <pravinkumar671231@gmail.com>
2025-06-17 10:48:50 +0530

    Fix getAllUsers API call and update baseURL

--- client/src/api.js
+++ client/src/api.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_KEY || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
   headers: { "Content-Type": "application/json" },
 });
 
 export const createUser = (email) => API.post("/users", { email });
 export const getAllUsers = () => API.get("/users");
+console.log(getAllUsers);
 
 export const sendMessageToAPI = (data) => API.post("/messages", data);
 export const getMessagesBetweenUsers = (user1, user2) =>
